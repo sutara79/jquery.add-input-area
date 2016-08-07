b78f3472e1aef01de97e55c05199e24518c576b6
Yuusaku Miyazaki <toumin.m7@gmail.com>
2016-08-07 11:00:49 +0900

    Add new demo

---
+++ demo/form-serialize.js
@@ -0,0 +1,12 @@
+$(function () {
+  $('.form-serialize').submit(function (ev) {
+    ev.preventDefault();
+    var arr = $(this).serializeArray();
+    console.log(arr);
+    var result = '';
+    $.each(arr, function (i, f) {
+      result += f.name + ': ' + f.value + '\n';
+    });
+    alert(result);
+  });
+});
